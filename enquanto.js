2e386eda233179b8026848e9bf68da5dbfdbac41
Rafael Rotela <rjvrafael22@gmail.com>
2026-01-10 11:03:17 -0300

    Aulas de laço de repetições

---
+++ enquanto.js
@@ -0,0 +1,14 @@
+// while ele verifica primeiro depois faz
+let contador = 1; 
+while (contador <= 10){
+    console.log("Número atual:", contador)
+    contador++;
+};
+
+let numero = 1 
+
+// O (do while), ele faz primeiro depois verifica
+do {
+    console.log('Número atual', numero)
+    numero++
+} while(numero <= 10)
